572fe2022f063b524498cc88b82903c73808b7ec
Hoa Tran <hoatran142@outlook.com>
2021-07-13 17:08:50 +0700

    three next python review files

---
+++ python_refresher/12_list_comprehensions.py
@@ -0,0 +1,11 @@
+numbers = [1, 3, 5]
+doubled = [x * 2 for x in numbers]
+
+
+friends = ["Sam", "Samantha", "Saurabh"]
+starts_s = [friend for friend in friends if friend.startswith("S")]
+
+print(friends)
+print(starts_s)
+print(friends is starts_s)
+print("friends: ", id(friends), "starts_s: ", id(starts_s))
